99b97eb9d10dd97d7c8e8f11dbf7a94992b7c220
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-12-06 15:40:49 +0000

    testing database post request for Username

--- server/app.js
+++ server/app.js
@@ -3,7 +3,14 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
- 
+
+require('./models/user')
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
     useUnifiedTopology: true
