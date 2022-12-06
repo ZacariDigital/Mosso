caa6dde1d58b221ff6fa6e455b5aa10744d07526
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-12-06 15:59:16 +0000

    Posting data to MongoDB Atlas
    Succesfully worked

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 router.get('/',(req,res)=>{
     res.send('hello')
@@ -8,9 +10,31 @@ router.get('/',(req,res)=>{
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
     if(!email || !password ||!name){
-        res.json({err:"please Input all the information"})
+        return res.json({err:"please Input all the information"})
     }
-    res.json({message:"successfully posted"})
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.json({err:"user already exist"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
+
 
 module.exports = router
