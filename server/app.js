408616a19bc91c83574ef410bc3686997dde4eec
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-12-06 14:49:33 +0000

    Connected Database to Mongo Atlas

--- server/app.js
+++ server/app.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = 5000
+//JoiRkPR5yk88YnPZ
 
-const customMiddleware = (req,res,next)=>{
-    console.log("middleware executed!")
-    next()
-}
 
-app.use(customMiddleware)
-
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("hello world")
-})
 
 app.listen(PORT,()=>{
     console.log('server is running on port',PORT)
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI : 'mongodb+srv://berman:JoiRkPR5yk88YnPZ@cluster0.8rkvff9.mongodb.net/?retryWrites=true&w=majority'
+}
