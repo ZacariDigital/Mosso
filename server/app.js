027196b4413c6cf8d2a03df72b84d86aa4a98ed4
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2022-12-06 14:56:33 +0000

    Connected to Mongo Cloud (atlas)
    testing connection and checking for errors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 5000
-//JoiRkPR5yk88YnPZ
-
-
+const {MONGOURI} = require('./keys')
+ 
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected', ()=>{
+    console.log('connected to mongoose')
+})
+mongoose.connection.on('error', ()=>{
+    console.log('err connecting', err)
+})
 
 app.listen(PORT,()=>{
     console.log('server is running on port',PORT)
