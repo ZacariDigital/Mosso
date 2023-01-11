6bd1bdcc20e13cec18954b96d15641da077b7b33
ZacariDigital <66054533+ZacariDigital@users.noreply.github.com>
2023-01-11 09:31:44 +0000

    implementing context api to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
 const NavBar = ()=>{
-    return(
+  const{state} = useContext(UserContext)
+  const renderList = ()=>{
+    if(state){
+      return[
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/findsong">Find Song</Link></li>
+
+      ]
+
+    }else{
+      return [
+        <li><Link to="/login">Login</Link></li>,
+        <li><Link to="/signup">Signup</Link></li>,
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/findsong">Find Song</Link></li>
+
+      ]
+
+    }
+  }
+  return(
   <nav>
   <div className="nav-wrapper black">
-    <Link to="/home" className="brand-logo left">Mosso</Link>
+    <Link to={"/home"}className="brand-logo left">Mosso</Link>
     <ul id="nav-mobile" className="right">
-      <li><Link to="/login">Login</Link></li>
-      <li><Link to="/signup">Signup</Link></li>
-      <li><Link to="/profile">Profile</Link></li>
+      {renderList()}
 
     </ul>
   </div>
